2ec93ffe65b6b17f41b53222af487f9c16b86577
Helen <zybina.e@mail.ru>
2023-01-12 20:01:41 +0300

    ДЗ c# задача 25

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число А:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+Double result=Math.Pow(A,B);
+Console.WriteLine(result);
